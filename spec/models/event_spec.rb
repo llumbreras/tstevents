fe848105eab37118b58688f166cb3f3098fa0b4f
L Lumbreras <llumbreras@gmail.com>
2013-06-04 14:41:33 -0700

    Modify the event spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -7,5 +7,15 @@ describe "Event" do
 
 		expect(event.free?).to eq(true)
 	end
+	it "displays the price if not $0" do
+		event = Event.new(price:10.00)
+
+		expect(event.free?).to eq(false)
+	end
+	it "is free if blank" do
+		event = Event.new(price:'')
+
+		expect(event.free?).to eq(true)
+	end
 end
 
